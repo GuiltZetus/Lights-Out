e467ffcab0a1895a5cd95af59459241b1e47b835
Nguyen Do Nhat Tan <personatan25@gmail.com>
2023-06-06 12:02:39 +0700

    added dragging item between inventory slots

--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -2,23 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
-public class InventoryScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class InventoryScript : MonoBehaviour, IDropHandler
 {
-    Transform parentAfterDrag;
-    public void OnBeginDrag (PointerEventData eventData)
+    public void OnDrop(PointerEventData eventData)
     {
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {   
-        transform.position = Input.mousePosition;
-    }
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        transform.SetParent(parentAfterDrag);
+        GameObject dropped = eventData.pointerDrag;
+        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        draggableItem.parentAfterDrag = transform;
     }
 }
